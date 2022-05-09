cf8ddcd7f4ffb512f85846c309e4e9fd34bc3fe2
Tultul <naher15-3882@diu.edu.bd>
2022-05-09 23:22:27 +0600

    Calculator

---
+++ Calculator1.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    cout << "what opperation you want to do : " ;
+
+    char ch ;
+
+    cin >> ch ;
+
+    switch (ch){
+
+        case '+' : cout << "Addition" ;
+
+        break;
+
+        case '-' : cout << "Substraction" ;
+
+        break ;
+
+        case '*' : cout << "Multiplication" ;
+
+        break;
+
+        case '/' : cout << "Division" ;
+
+        break;
+
+        default : cout << "Error" ;
+    }
+
+
+
+    return 0;
+}
